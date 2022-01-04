6b7c80a25c188370ed0ae8f228d7f2c50b2cde1a
AndinaZS <alexa.markova@outlook.com>
2022-01-04 20:08:59 +0300

    added regex

--- app.py
+++ app.py
@@ -1,53 +1,69 @@
 import os
-
+import re
+from typing import Generator, List, Set
+from dataclasses import dataclass
 from flask import Flask, request
+import marshmallow_dataclass
+import marshmallow
 
 app = Flask(__name__)
+
+@dataclass
+class MyRequest:
+    file_name: str
+    cmd1: str
+    value1: str
+    cmd2: str
+    value2: str = ''
+
+    class Meta:
+        unknown = marshmallow.EXCLUDE
+
+MyRequest_Schema = marshmallow_dataclass.class_schema(MyRequest)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
-def file_reader(file_name):
+def file_reader(file_name: str) -> Generator:
     with open(file_name) as f:
         for row in f:
             yield row
 
-def filter_func(data, param:str):
+def filter_func(data, param: str) -> filter:
     return filter(lambda x: param in x, data)
 
-def map_func(data, param:str):
+def map_func(data, param: str) -> map:
     return map(lambda x: f'{x[int(param)]}\n', [el.split() for el in data])
 
-def sorted_func(data, param:str):
+def sorted_func(data, param: str) -> List:
     return sorted(data, reverse=True if param == 'desc' else False)
 
-def limit(data, param):
+def limit(data, param: str) -> List:
     return [next(data) for _ in range(int(param))]
 
-def unique(data, param=''):
+def unique(data, param: str ='') -> Set:
     return set(data)
+
+def regex(data, param: str) -> Generator:
+    for d in data:
+        if re.findall(re.compile(param), d):
+            yield d
+
 
 FUNC_MAPPING={'filter': filter_func,
               'map': map_func,
               'sort': sorted_func,
               'unique': unique,
-              'limit': limit,}
+              'limit': limit,
+              'regex': regex}
 
 @app.route("/perform_query", methods=['POST'])
 def perform_query():
-    res = request.form.to_dict()
-    path = f'{DATA_DIR}/{res["file_name"]}'
+    my_request = MyRequest_Schema().load(request.form.to_dict())
+    path = f'{DATA_DIR}/{my_request.file_name}'
     if not os.path.exists(path):
         return 'error: file not found', 400
-    try:
-        cmd1, value1, cmd2, value2 = res['cmd1'], res['value1'], res['cmd2'], res['value2']
-    except KeyError:
-        return 'error: something wrong with parameters', 400
     data = file_reader(path)
-    try:
-        data = FUNC_MAPPING[cmd1](data, value1)
-        data = FUNC_MAPPING[cmd2](data, value2)
-    except KeyError:
-        return 'error: invalid parameters', 400
-
+    data = FUNC_MAPPING[my_request.cmd1](data, my_request.value1)
+    data = FUNC_MAPPING[my_request.cmd2](data, my_request.value2)
     return app.response_class(data, content_type="text/plain")
